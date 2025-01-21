600fb13bb6f8d77a53bca7cfa310b03b67a09d96
Christian Simonsen <crsdev@gmail.com>
2025-01-21 12:29:47 +0100

    Utgangspunklt for unit testing

---
+++ test/test.mjs
@@ -0,0 +1,39 @@
+let padding = 0;
+let totaltTime = 0;
+
+
+function Test(description) {
+
+    console.log(description)
+
+    console.time("Test");
+
+    return {
+        isEqual,
+        padd: (n = 1) => { padding += n; },
+        removePadd: () => {
+            padding--;
+            if (padding < 0) {
+                padding = 0;
+            }
+        },
+        end: () => {
+            console.timeEnd("Test");
+        }
+
+    }
+}
+
+function isEqual(recived, expected, description) {
+    if (recived === expected) {
+        console.log(`${addPadd(padding)}🟢 ${description}`);
+    } else {
+        console.log(`${addPadd(padding)}🔴 ${description}, forventett ${expected}, fikk ${recived}`);
+    }
+}
+
+function addPadd(padding) {
+    return "".padStart(padding, " ");
+}
+
+export default Test
